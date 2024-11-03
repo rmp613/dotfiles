bc3cb417ccfc87d5efa9dffa034e9ae00e6a1556
Riordan Pawley <riordanpawley@gmail.com>
2024-11-04 00:28:33 +1100

    back to yazi

---
+++ home/cli/yazi.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+# let
+  # yazi-plugins = pkgs.fetchFromGitHub {
+  #   owner = "yazi-rs";
+  #   repo = "plugins";
+  #   rev = "...";
+  #   hash = "sha256-...";
+  # };
+# in
+{
+  programs.yazi = {
+    enable = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+
+    shellWrapperName = "y";
+
+  #   starship = pkgs.fetchFromGitHub {
+  #     owner = "Rolv-Apneseth";
+  #     repo = "starship.yazi";
+  #     rev = "...";
+  #     sha256 = "sha256-...";
+  #   };
+  #   initLua = ''
+		# 	require("starship"):setup()
+		# '';
+  };
+}
